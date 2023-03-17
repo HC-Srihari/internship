f64aa7796426740242f2f646f2bd08a502e83958
HC-Srihari <126495426+HC-Srihari@users.noreply.github.com>
2023-03-17 23:35:17 +0530

    final sql file
    sql file after the completion of the project.
    contains latest info in the database

--- myblog_db.sql
+++ myblog_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2022 at 09:42 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Mar 17, 2023 at 06:14 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `categories` (
   `category` varchar(50) NOT NULL,
   `slug` varchar(100) NOT NULL,
   `disabled` tinyint(4) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `categories`
@@ -60,7 +60,7 @@ CREATE TABLE `posts` (
   `image` varchar(1024) DEFAULT NULL,
   `date` datetime DEFAULT current_timestamp(),
   `slug` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
@@ -70,7 +70,8 @@ INSERT INTO `posts` (`id`, `user_id`, `category_id`, `title`, `content`, `image`
 (1, 1, 4, 'My first post', 'This is some edited content', 'uploads/1665481341530dd282a061b9e825a122f61d59c668.jpg', '2022-10-11 11:42:21', 'my-first-post'),
 (3, 1, 2, 'My second post', '<ol><li><h2>my old images here</h2></li></ol><p>formated content<br></p><p><img style=\"width: 50%;\" src=\"////uploads/Billy-Joe-Armstrong.jpg\" data-filename=\"Billy-Joe-Armstrong.jpg\"></p><p><br></p><h4><font color=\"#FF00FF\">some text here</font><br></h4><p><img style=\"width: 100%;\" src=\"//uploads/7xuuL9GAGDDjhietMy3RGJ.jpg\" data-filename=\"7xuuL9GAGDDjhietMy3RGJ.jpg\"><br></p><p><br></p>', 'uploads/1665482550blog-post-image-guide.jpg', '2022-10-11 12:02:30', 'my-second-post'),
 (4, 1, 5, 'My life this july', 'this shows my work', 'uploads/1665482588why-INFJs-should-start-a-blog-770x470-1.jpg', '2022-10-11 12:03:09', 'my-life-this-july'),
-(5, 1, 2, 'a post in crime', 'some content', 'uploads/16654937895454078.jpg', '2022-10-11 15:09:49', 'a-post-in-crime');
+(5, 1, 2, 'a post in crime', 'some content', 'uploads/16654937895454078.jpg', '2022-10-11 15:09:49', 'a-post-in-crime'),
+(6, 8, 5, 'location', '<p>dkjsf;gkjfghjfklg</p>', 'uploads/1679070960location.jpg', '2023-03-17 22:06:00', 'location');
 
 -- --------------------------------------------------------
 
@@ -86,7 +87,7 @@ CREATE TABLE `users` (
   `image` varchar(1024) DEFAULT NULL,
   `date` datetime DEFAULT current_timestamp(),
   `role` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -97,7 +98,8 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `image`, `date`, `ro
 (3, 'new', 'my@mail.com', '$2y$10$TYxT.0rztSPztkTNLrg.WOo/kymSeJmw3I38I/kNVtxR98qLWOykq', 'uploads/16652679461c59efd01451b880.jpg', '2022-10-09 00:00:42', 'user'),
 (4, 'Edd', 'ed@email.com', '$2y$10$cmv2an.7ZrH.c2I9uOYS5eoyVR/TpVBOUODGUSOUbZxrOkjqKUxM2', 'uploads/1665510020alicia-keys.jpg', '2022-10-09 00:29:37', 'user'),
 (5, 'alisha', 'alisha@email.com', '$2y$10$t3dPuL.SY/gKbmbELcK3Vu98ycwd1SQ0xafJDkDi1sr9ojiAC5DeG', 'uploads/1665268261alicia-keys.jpg', '2022-10-09 00:31:01', 'admin'),
-(7, 'Mary', 'mary@email.com', '$2y$10$afyuLEHL1MGiQ0zhXK3wdOA4pXljclUwbX2g2uZUq.Zhlt24x2o/q', 'uploads/1665509556amifaku.jpg', '2022-10-11 19:32:36', 'user');
+(7, 'Mary', 'mary@email.com', '$2y$10$afyuLEHL1MGiQ0zhXK3wdOA4pXljclUwbX2g2uZUq.Zhlt24x2o/q', 'uploads/1665509556amifaku.jpg', '2022-10-11 19:32:36', 'user'),
+(8, 'hari', 'sriharihc733@gmail.com', '$2y$10$vkXUq0qj8FZldU7t/hL45OG26yuYkIKvR.bRnEQ5bCjOcCyHzJEay', NULL, '2023-03-17 21:36:13', 'user');
 
 --
 -- Indexes for dumped tables
@@ -144,13 +146,13 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
